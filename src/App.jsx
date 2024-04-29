19458a0781e43ca075bda566c522a3e43488cf09
Nelson Mbau <mbau.ndungu@gmail.com>
2024-04-29 07:07:47 +0300

    feat: set state for filtered transactions and search
    Manage state for filtered transactions and search, and add effect to filter transactions on search change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import {
   Header,
@@ -12,19 +12,36 @@ const genId = () => {
 };
 
 function App() {
+  const [search, setSearch] = useState("");
   const [transactions, setTransactions] = useState([]);
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
 
-  const onSubmitHandler = (evt, formState) => {
-    evt.preventDefault();
+  const onSubmitHandler = (formState) => {
     setTransactions((prev) => [...prev, { ...formState, id: genId() }]);
   };
+
+  const onSearchCange = (evt) => {
+    setSearch(evt.target.value);
+  };
+
+  useEffect(() => {
+    if (search.length === 0) {
+      setFilteredTransactions(transactions);
+    } else {
+      setFilteredTransactions(
+        transactions.filter((transaction) =>
+          transaction.description.toLowerCase().includes(search.toLowerCase())
+        )
+      );
+    }
+  }, [search, transactions]);
 
   return (
     <div>
       <Header title="Bank Of Flatiron" />
-      <SearchInput />
+      <SearchInput value={search} onChange={onSearchCange} />
       <TransactionForm onSubmit={onSubmitHandler} />
-      <TransactionTable transactions={transactions} />
+      <TransactionTable transactions={filteredTransactions} />
     </div>
   );
 }
