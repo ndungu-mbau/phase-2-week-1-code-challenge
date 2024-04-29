ded74d5053ff1c7693306e5a72905fb2757fea14
Nelson Mbau <mbau.ndungu@gmail.com>
2024-04-29 07:06:36 +0300

    feat: pass value and onChange prop to manage input state

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,11 +1,20 @@
-export const SearchInput = () => {
+import PropTypes from "prop-types";
+
+export const SearchInput = ({ value, onChange }) => {
   return (
     <div className="flex justify-center mb-4 flex-1">
       <input
         type="text"
         placeholder="Search"
         className="w-full shadow border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline m-8"
+        value={value}
+        onChange={onChange}
       />
     </div>
   );
+};
+
+SearchInput.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
